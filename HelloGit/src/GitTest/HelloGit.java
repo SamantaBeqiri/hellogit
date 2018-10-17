cf881648513f1ceafbb898ad9e764d527eceeb8a
Dajana <dajana_muho@yahoo.com>
2018-10-17 21:51:54 +0300

    Lol

--- HelloGit/src/GitTest/HelloGit.java
+++ HelloGit/src/GitTest/HelloGit.java
@@ -9,8 +9,7 @@ public class HelloGit {
 System.out.println("Hello,git");
 System.out.println("Dajana I love youuu");
 System.out.println("Me neither");
-System.out.println("La");
-System.out.println(2);
+
 
 
 
