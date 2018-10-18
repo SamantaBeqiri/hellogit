7e365debb158bef8e1bba6f0cab3d6b55c0185c6
Samanta <samanta29beqiri@gmail.com>
2018-10-18 14:52:36 +0200

    changed

--- HelloGit/src/GitTest/HelloGit.java
+++ HelloGit/src/GitTest/HelloGit.java
@@ -4,24 +4,16 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//comentadded
-		//coment
-System.out.println("Hello,git");
-System.out.println("Dajana I love youuu");
-System.out.println("Lalala");
-System.out.println("Me neither");
-System.out.println("Me neither");
-System.out.println("Na cateee koken ,na e cave ti ");
-int i=0;
-int a=1;
-while (i<3) {
+		
+		int []data= {-1,2,3};
 	
-	System.out.println(a);
-}
-System.out.println(2);
+		for(int i:data  ) {
+			System.out.println(data[i]);
+		}
+		
 
 
-
+	
 
 
 
