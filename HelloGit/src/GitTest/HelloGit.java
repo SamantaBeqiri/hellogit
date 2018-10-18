1aa76fb47b70b41c3102a5cc5808cd5be2b5ace9
Samanta <samanta29beqiri@gmail.com>
2018-10-18 09:28:45 +0200

    hey

--- HelloGit/src/GitTest/HelloGit.java
+++ HelloGit/src/GitTest/HelloGit.java
@@ -12,7 +12,7 @@ System.out.println("Lalala");
 System.out.println("Me neither");
 System.out.println("Me neither");
 System.out.println("Really :p");
-System.out.println("Na cat koken ");
+System.out.println("Na cat koken ,na e cave ti ");
 
 
 
